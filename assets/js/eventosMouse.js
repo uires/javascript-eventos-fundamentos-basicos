3301252e96b30cfc4e1262f659f5925203dc8754
uires <uiresdeivide1@gmail.com>
2018-01-22 20:59:30 -0200

    Eventos de mouse e conhecendo eventos de teclado

---
+++ assets/js/eventosMouse.js
@@ -0,0 +1,21 @@
+function holderMouse() {
+	
+	var cast = document.getElementById('div-teste').style.backgroundColor = "black";	
+	var cast = document.getElementById('div-teste').style.backgroundImage = "url('https://www.google.org/assets/static/images/logo_googledotorg-171e7482e5523603fc0eed236dd772d8.svg')";
+
+}
+
+function soltou(){
+	console.log('Tirou o  cursors... '); 
+	var cast = document.getElementById('div-teste').style.backgroundColor = "green";
+	var cast = document.getElementById('div-teste').style.backgroundImage = "url('https://www.google.org/assets/static/images/logo_googledotorg-171e7482e5523603fc0eed236dd772d8.svg')";
+}
+
+function releaseCursor(){
+	console.log('Soltou o mouse... ');
+	var cast = document.getElementById('div-teste').style.backgroundImage = "url('https://www.google.org/assets/static/images/logo_googledotorg-171e7482e5523603fc0eed236dd772d8.svg')";
+}
+
+function block() {
+	alert('can do it');
+}
